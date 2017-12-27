7196da09b9a51794448e1a9763c2070ebd530443
yedawang <ye_dawang123@163.com>
2017-12-27 11:31:51 +0800

    Spring Cloud config

---
+++ spring-cloud-config-server-git/src/main/java/com/chinasofti/configServerGit/SpringCloudConfigServerGitApplication.java
@@ -0,0 +1,14 @@
+package com.chinasofti.configServerGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerGitApplication.class, args);
+	}
+}
